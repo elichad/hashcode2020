d00874122b679e02f7562f8ed2946378cca5c3fc
Eli Chadwick <eli.chadwick@stfc.ac.uk>
2020-02-20 19:00:24 +0000

    Added structures

---
+++ structures.py
@@ -0,0 +1,43 @@
+class Book: #unused?
+    def __init__(self, id, score):
+        self.id = id
+        self.score = score
+
+class Library:
+    def __init__(self, id, n_books, signup, scans, books):
+        self.id = id
+        self.n_books = n_books
+        self.signup = signup
+        self.scans = scans
+        self.books = books #dict {id: score}
+
+    def get_total_score(self):
+        tot = 0
+        for id in self.books.keys():
+            tot += self.books[id]
+        return tot
+
+    def get_todays_books(self,all_scanned):
+        ids = set()
+        while len(ids) < self.scans:
+            best_id = max(self.books.keys(), key=lambda k: self.books[k])
+            if best_id not in all_scanned:
+                ids.add(best_id)
+            del self.books[best_id] #always delete
+        return ids
+        #after this in main code: want all_scanned.add(ids)
+                
+        
+#dict((k, bigdict[k]) for k in ('l', 'm', 'n'))
+#initialise with : Library.books = dict((k, all_books[k] for k in lib_book_ids))    
+
+
+
+if __name__=="__main__":
+    books = {0: 10,1:20}
+    libs = {0: Library(0,2,1,2,{0:10,1:20})}
+    scanned = set()
+    for lib_id in libs:
+        lib = libs[lib_id]
+        print(lib_id, lib.get_total_score(), lib.get_todays_books(scanned), lib.get_total_score()) #very well written test: 0 30 {0,1} 0 (but book 1 is scanned before 0)
+
