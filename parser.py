c964afdacddbe4b048ed1c54b2a5fbd04242df63
esl-lewis <emzlewis@hotmail.co.uk>
2020-02-20 20:27:55 +0000

    Complete parser

--- parser.py
+++ parser.py
@@ -11,10 +11,10 @@ def get_data(filename="input.txt"):
         # print(first_line)
 
         total_books = int(first_line[0])
-        total_libs = int(first_line[1])
+        total_libraries = int(first_line[1])
         total_days = int(first_line[2])
 
-        print(total_books,total_libs,total_days)
+        # print(total_books,total_libs,total_days)
 
         book_scores = str(lines[1])
         book_scores = book_scores.split()
@@ -24,25 +24,51 @@ def get_data(filename="input.txt"):
         book_id_dict = {}
 
         for book in range(0,total_books):
-            print(book, book_scores[book])
-            # book_id = book
-            # book_score = book_scores[book]
-            # book_id_dict[book_id]=book_score
+            # print(book, book_scores[book])
+            book_id = book
+            book_score = book_scores[book]
+            book_id_dict[book_id]=book_score
 
-    #     print(type(lines))
+        #  print(type(lines))
         # print(book_id_dict)
 
-    # del lines[0:2]
+    del lines[0:2]
 
-    # libraries = lines
-    
-    # library_dict = {}
-    
-    # for 
+    libraries = lines
+    # print(libraries)
+    list_libraries = []
 
-    
+    for library in range(0, len(libraries), 2):
+        # print(library)
 
-    # return(total_books,total_libs,total_days,book_id_dict)
+        library_info = str(libraries[library])
+        library_info = library_info.split()
+
+        num_books_in_lib = library_info[0]
+        signup_process_days = library_info[1]
+        books_per_day = library_info[2]
+    
+        books_in_lib = str(libraries[library+1])
+        books_in_lib = books_in_lib.split()
+        # print(books_in_lib)
+        # print("length",len(books_in_lib))
+
+        book_lib_dict = {}
+
+        for book in range(0,len(books_in_lib)):
+
+            # print(book, book_scores[book])
+            book_id = book
+            # print(book_id)
+            # print(books_in_lib[book])
+            book_score = books_in_lib[book]
+            book_lib_dict[book_id]=book_score
+        library_id = library/2
+        # print(book_lib_dict)
+        library_obj = Library(library_id, num_books_in_lib,signup_process_days, books_per_day, book_lib_dict )
+        list_libraries.append(library_obj)
+    # print(list_libraries)
+    return(total_books,total_libraries,total_days,book_id_dict, list_libraries)
 
 
 get_data('a_example.txt')
