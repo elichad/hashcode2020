410bc73f322ca53e7eae1aaf25a0192a9a09427a
Eli Chadwick <eli.chadwick.256@gmail.com>
2020-02-20 20:44:37 +0000

    Use ordereddict

--- output.py
+++ output.py
@@ -1,15 +1,15 @@
 #expect output in form
 #libs = {id: [ordered_book_ids]}, [ordered_libs]
+from collections import OrderedDict
 
-def gen_output(libs,order):
+def gen_output(libs):
     with open("out.txt",'w') as out:
-        out.write("{}\n".format(len(order)))
-        for lib_id in order:
+        out.write("{}\n".format(len(libs.keys())))
+        for lib_id in libs.keys():
             book_list = libs[lib_id]
             out.write("{} {}\n".format(lib_id,len(book_list)))
             out.write(" ".join([str(b) for b in book_list])+"\n")
 
 if __name__=="__main__":
-    libs = {0: [5,2,587,6], 1:[3,4,5]} #test content
-    order = [1,0]
-    gen_output(libs, order)
+    libs = OrderedDict({0: [5,2,587,6], 1:[3,4,5]}) #test content
+    gen_output(libs)
